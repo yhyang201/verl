f7f71ec807b0d64169f6ccee3b6e04237ce9edb3
Mayflyyh <yhyang201@gmail.com>
2025-04-07 20:05:38 +0800

    init

---
+++ recipe/sppo/src/dp_actor.py
@@ -0,0 +1,152 @@
+import torch
+
+
+from verl import DataProto
+from verl.trainer.ppo.core_algos import compute_policy_loss, kl_penalty, agg_loss
+from verl.utils.py_functional import append_to_dict
+
+from verl.utils.seqlen_balancing import rearrange_micro_batches, get_reverse_idx
+
+import verl.utils.torch_functional as verl_F
+
+def compute_sppo_loss(
+    old_log_prob: torch.Tensor,      # (bs, seq_len)
+    log_prob: torch.Tensor,          # (bs, seq_len)
+    rewards: torch.Tensor,        # (bs,)
+    response_mask: torch.Tensor,     # (bs, seq_len)
+    eta: float = 1.0,
+    loss_agg_mode: str = "token-mean"
+):
+    """
+    SPPO Loss computation.
+    """
+    # Compute log-ratios over masked tokens
+    log_prob_sum = (log_prob * response_mask).sum(dim=1)  # (bs,)
+    old_log_prob_sum = (old_log_prob * response_mask).sum(dim=1)  # (bs,)
+    log_ratios = log_prob_sum - old_log_prob_sum  # (bs,)
+
+    preference = eta * (response_mask - 0.5)  # (bs,)
+    loss_vec = (log_ratios - rewards) ** 2  # (bs,)
+
+    if loss_agg_mode == "seq-mean-token-sum":
+        loss = loss_vec.mean()
+    elif loss_agg_mode == "seq-mean-token-mean":
+        seq_lengths = response_mask.sum(dim=1)  # (bs,)
+        token_mean_loss = loss_vec / seq_lengths.clamp(min=1)
+        loss = token_mean_loss.mean()
+    elif loss_agg_mode == "token-mean":
+        sample_mask = response_mask.any(dim=1).float()  # (bs,)
+        loss = verl_F.masked_mean(loss_vec, sample_mask)       
+    else:
+        raise ValueError(f"Unsupported loss_agg_mode: {loss_agg_mode}")
+
+    return loss, log_ratios, preference
+
+
+def update_policy(self, data: DataProto):
+    # make sure we are in training mode
+    self.actor_module.train()
+
+    temperature = data.meta_info['temperature']  # temperature must be in the data.meta_info to avoid slient error
+
+    select_keys = ['responses', 'input_ids', 'attention_mask', 'position_ids', 'old_log_probs', 'advantages']
+    if self.config.use_kl_loss:
+        select_keys.append('ref_log_prob')
+    batch = data.select(batch_keys=select_keys).batch
+    has_multi_modal_inputs = 'multi_modal_inputs' in data.non_tensor_batch.keys()
+
+    # Split to make minibatch iterator for updating the actor
+    # See PPO paper for details. https://arxiv.org/abs/1707.06347
+    if has_multi_modal_inputs:
+        num_mini_batches = data.batch.batch_size[0] // self.config.ppo_mini_batch_size
+        non_tensor_select_keys = ['multi_modal_inputs']
+        dataloader = data.select(select_keys, non_tensor_select_keys).chunk(num_mini_batches)
+    else:
+        dataloader = batch.split(self.config.ppo_mini_batch_size)
+
+    metrics = {}
+    for epoch in range(self.config.ppo_epochs):
+        for batch_idx, data in enumerate(dataloader):
+            # split batch into micro_batches
+            mini_batch = data
+            if has_multi_modal_inputs:
+                self.gradient_accumulation = self.config.ppo_mini_batch_size // self.config.ppo_micro_batch_size_per_gpu
+                num_micro_batches = mini_batch.batch.batch_size[0] // self.config.ppo_micro_batch_size_per_gpu
+                micro_batches = data.select(select_keys, non_tensor_select_keys).chunk(num_micro_batches)
+            elif self.config.use_dynamic_bsz:
+                max_token_len = self.config.ppo_max_token_len_per_gpu * self.ulysses_sequence_parallel_size
+                micro_batches, _ = rearrange_micro_batches(batch=mini_batch, max_token_len=max_token_len)
+            else:
+                self.gradient_accumulation = self.config.ppo_mini_batch_size // self.config.ppo_micro_batch_size_per_gpu
+                # split batch into micro_batches
+                micro_batches = mini_batch.split(self.config.ppo_micro_batch_size_per_gpu)
+
+            self.actor_optimizer.zero_grad()
+
+            for data in micro_batches:
+                # Support all hardwares
+                if isinstance(data, DataProto):
+                    data = {**data.batch.to(torch.cuda.current_device()), **data.non_tensor_batch}
+                else:
+                    data = data.to(torch.cuda.current_device())  # actor device is cpu when using offload
+                responses = data['responses']
+                response_length = responses.size(1)
+                attention_mask = data['attention_mask']
+                response_mask = attention_mask[:, -response_length:]
+                old_log_prob = data['old_log_probs']
+                rewards = data['token_level_rewards']      
+                          
+                entropy_coeff = self.config.entropy_coeff
+                loss_agg_mode = self.config.loss_agg_mode
+
+                # all return: (bsz, response_length)
+                entropy, log_prob = self._forward_micro_batch(micro_batch=data, temperature=temperature)
+
+                pg_loss, log_ratios, preference = compute_sppo_loss(
+                    old_log_prob=old_log_prob,
+                    log_prob=log_prob,
+                    rewards=rewards,
+                    response_mask=response_mask,
+                    eta=self.config.get('sppo_eta', 1.0),
+                    loss_agg_mode=loss_agg_mode,
+                )
+                # compute entropy loss from entropy
+                entropy_loss = agg_loss(loss_mat=entropy, loss_mask=response_mask, loss_agg_mode=loss_agg_mode)
+
+                # compute policy loss
+                policy_loss = pg_loss - entropy_loss * entropy_coeff
+
+                if self.config.use_kl_loss:
+                    ref_log_prob = data['ref_log_prob']
+                    # compute kl loss
+                    kld = kl_penalty(logprob=log_prob,
+                                        ref_logprob=ref_log_prob,
+                                        kl_penalty=self.config.kl_loss_type)
+                    kl_loss = agg_loss(loss_mat=kld,
+                                        loss_mask=response_mask,
+                                        loss_agg_mode=self.config.loss_agg_mode)
+
+                    policy_loss = policy_loss + kl_loss * self.config.kl_loss_coef
+                    metrics['actor/kl_loss'] = kl_loss.detach().item()
+                    metrics['actor/kl_coef'] = self.config.kl_loss_coef
+
+                if self.config.use_dynamic_bsz:
+                    # relative to the dynamic bsz
+                    loss = policy_loss * (len(data) / self.config.ppo_mini_batch_size)
+                else:
+                    loss = policy_loss / self.gradient_accumulation
+                loss.backward()
+
+                data_metrics = {
+                    'actor/entropy': entropy_loss.detach().item(),
+                    'actor/pg_loss': pg_loss.detach().item(),
+                    'actor/log_ratio_mean': log_ratios.mean().detach().item(),
+                    'actor/preference_mean': preference.mean().detach().item(),
+                }
+                append_to_dict(metrics, data)
+
+            grad_norm = self._optimizer_step()
+            data = {'actor/grad_norm': grad_norm.detach().item()}
+        append_to_dict(metrics, data)
+    self.actor_optimizer.zero_grad()
+    return metrics
---
+++ recipe/sppo/src/sppo_ray_trainer.py
@@ -0,0 +1,204 @@
+"""
+FSDP PPO Trainer with Ray-based single controller.
+This trainer supports model-agonistic model initialization with huggingface
+"""
+
+import uuid
+from pprint import pprint
+from copy import deepcopy
+from collections import defaultdict
+from tqdm import tqdm
+import numpy as np
+import torch
+
+from verl import DataProto
+from verl.single_controller.base import Worker
+from verl.single_controller.base.decorator import register, Dispatch
+from verl.trainer.ppo.ray_trainer import RayPPOTrainer, _timer, apply_kl_penalty, compute_advantage, AdvantageEstimator, compute_response_mask
+from verl.trainer.ppo.metric_utils import (compute_data_metrics, compute_throughout_metrics, compute_timing_metrics,
+                                           reduce_metrics)
+
+    
+class RaySPPOTrainer(RayPPOTrainer):
+    def fit(self):
+        """
+        The training loop of PPO.
+        The driver process only need to call the compute functions of the worker group through RPC to construct the PPO dataflow.
+        The light-weight advantage computation is done on the driver process.
+        """
+        from verl.utils.tracking import Tracking
+        from omegaconf import OmegaConf
+
+        logger = Tracking(project_name=self.config.trainer.project_name,
+                          experiment_name=self.config.trainer.experiment_name,
+                          default_backend=self.config.trainer.logger,
+                          config=OmegaConf.to_container(self.config, resolve=True))
+
+        self.global_steps = 0
+
+        # load checkpoint before doing anything
+        self._load_checkpoint()
+
+        # perform validation before training
+        # currently, we only support validation using the reward_function.
+        if self.val_reward_fn is not None and self.config.trainer.get('val_before_train', True):
+            val_metrics = self._validate()
+            pprint(f'Initial validation metrics: {val_metrics}')
+            logger.log(data=val_metrics, step=self.global_steps)
+            if self.config.trainer.get('val_only', False):
+                return
+
+        # add tqdm
+        progress_bar = tqdm(total=self.total_training_steps, initial=self.global_steps, desc="Training Progress")
+
+        # we start from step 1
+        self.global_steps += 1
+        last_val_metrics = None
+
+        for epoch in range(self.config.trainer.total_epochs):
+            for batch_dict in self.train_dataloader:
+                metrics = {}
+                timing_raw = {}
+
+                batch: DataProto = DataProto.from_single_dict(batch_dict)
+
+                # pop those keys for generation
+                if 'multi_modal_inputs' in batch.non_tensor_batch.keys():
+                    gen_batch = batch.pop(
+                        batch_keys=['input_ids', 'attention_mask', 'position_ids'],
+                        non_tensor_batch_keys=['raw_prompt_ids', 'multi_modal_data', 'multi_modal_inputs'],
+                    )
+                else:
+                    gen_batch = batch.pop(
+                        batch_keys=['input_ids', 'attention_mask', 'position_ids'],
+                        non_tensor_batch_keys=['raw_prompt_ids'],
+                    )
+
+                is_last_step = self.global_steps >= self.total_training_steps
+
+                with _timer('step', timing_raw):
+                    # generate a batch
+                    with _timer('gen', timing_raw):
+                        gen_batch_output = self.actor_rollout_wg.generate_sequences(gen_batch)
+
+                    batch.non_tensor_batch['uid'] = np.array([str(uuid.uuid4()) for _ in range(len(batch.batch))],
+                                                             dtype=object)
+                    # repeat to align with repeated responses in rollout
+                    batch = batch.repeat(repeat_times=self.config.actor_rollout_ref.rollout.n, interleave=True)
+                    batch = batch.union(gen_batch_output)
+
+                    batch.batch['response_mask'] = compute_response_mask(batch)
+                    # balance the number of valid tokens on each dp rank.
+                    # Note that this breaks the order of data inside the batch.
+                    # Please take care when you implement group based adv computation such as GRPO and rloo
+                    if self.config.trainer.balance_batch:
+                        self._balance_batch(batch, metrics=metrics)
+
+                    # compute global_valid tokens
+                    batch.meta_info['global_token_num'] = torch.sum(batch.batch['attention_mask'], dim=-1).tolist()
+
+                    # recompute old_log_probs
+                    with _timer('old_log_prob', timing_raw):
+                        old_log_prob = self.actor_rollout_wg.compute_log_prob(batch)
+                        batch = batch.union(old_log_prob)
+
+                    if self.use_reference_policy:
+                        # compute reference log_prob
+                        with _timer('ref', timing_raw):
+                            ref_log_prob = self.ref_policy_wg.compute_ref_log_prob(batch)
+                            batch = batch.union(ref_log_prob)
+
+                    # compute values
+                    # if self.use_critic:
+                    #     with _timer('values', timing_raw):
+                    #         values = self.critic_wg.compute_values(batch)
+                    #         batch = batch.union(values)
+
+                    with _timer('adv', timing_raw):
+                        # compute scores. Support both model and function-based.
+                        # We first compute the scores using reward model. Then, we call reward_fn to combine
+                        # the results from reward model and rule-based results.
+                        if self.use_rm:
+                            # we first compute reward model score
+                            reward_tensor = self.rm_wg.compute_rm_score(batch)
+                            batch = batch.union(reward_tensor)
+
+                        # we combine with rule-based rm
+                        reward_extra_infos_dict: dict[str, list]
+                        try:
+                            reward_result = self.reward_fn(batch, return_dict=True)
+                            reward_tensor = reward_result['reward_tensor']
+                            reward_extra_infos_dict = reward_result['reward_extra_info']
+                        except Exception as e:
+                            print(f'Error in reward_fn: {e}')
+                            reward_tensor = self.reward_fn(batch)
+                            reward_extra_infos_dict = {}
+
+                        batch.batch['token_level_scores'] = reward_tensor
+
+                        print(f'{list(reward_extra_infos_dict.keys())=}')
+                        if reward_extra_infos_dict:
+                            batch.non_tensor_batch.update({k: np.array(v) for k, v in reward_extra_infos_dict.items()})
+
+                        # compute rewards. apply_kl_penalty if available
+                        if self.config.algorithm.use_kl_in_reward:
+                            batch, kl_metrics = apply_kl_penalty(batch,
+                                                                 kl_ctrl=self.kl_ctrl_in_reward,
+                                                                 kl_penalty=self.config.algorithm.kl_penalty)
+                            metrics.update(kl_metrics)
+                        else:
+                            batch.batch['token_level_rewards'] = batch.batch['token_level_scores']
+
+                        # compute advantages, executed on the driver process
+                        # batch = compute_advantage(batch,
+                        #                           adv_estimator=self.config.algorithm.adv_estimator,
+                        #                           gamma=self.config.algorithm.gamma,
+                        #                           lam=self.config.algorithm.lam,
+                        #                           num_repeat=self.config.actor_rollout_ref.rollout.n)
+
+                    # update critic
+                    # if self.use_critic:
+                    #     with _timer('update_critic', timing_raw):
+                    #         critic_output = self.critic_wg.update_critic(batch)
+                    #     critic_output_metrics = reduce_metrics(critic_output.meta_info['metrics'])
+                    #     metrics.update(critic_output_metrics)
+
+                    # implement critic warmup
+                    if self.config.trainer.critic_warmup <= self.global_steps:
+                        # update actor
+                        with _timer('update_actor', timing_raw):
+                            actor_output = self.actor_rollout_wg.update_actor(batch)
+                        actor_output_metrics = reduce_metrics(actor_output.meta_info['metrics'])
+                        metrics.update(actor_output_metrics)
+
+                    # validate
+                    if self.val_reward_fn is not None and self.config.trainer.test_freq > 0 and \
+                        (is_last_step or  self.global_steps % self.config.trainer.test_freq == 0):
+                        with _timer('testing', timing_raw):
+                            val_metrics: dict = self._validate()
+                            if is_last_step:
+                                last_val_metrics = val_metrics
+                        metrics.update(val_metrics)
+
+                    if self.config.trainer.save_freq > 0 and ( is_last_step or \
+                            self.global_steps % self.config.trainer.save_freq == 0):
+                        with _timer('save_checkpoint', timing_raw):
+                            self._save_checkpoint()
+
+                # collect metrics
+                metrics.update(compute_data_metrics(batch=batch, use_critic=self.use_critic))
+                metrics.update(compute_timing_metrics(batch=batch, timing_raw=timing_raw))
+                # TODO: implement actual tflpo and theoretical tflpo
+                n_gpus = self.resource_pool_manager.get_n_gpus()
+                metrics.update(compute_throughout_metrics(batch=batch, timing_raw=timing_raw, n_gpus=n_gpus))
+
+                # TODO: make a canonical logger that supports various backend
+                logger.log(data=metrics, step=self.global_steps)
+
+                if is_last_step:
+                    pprint(f'Final validation metrics: {last_val_metrics}')
+                    progress_bar.close()
+                    return
+
+                progress_bar.update(1)
+                self.global_steps += 1
